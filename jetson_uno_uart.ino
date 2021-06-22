afc512074201d5dbe11a35e878a15c09f3d354c3
ky-devblog <53827090+ky-devblog@users.noreply.github.com>
2021-06-22 13:37:13 +0900

    comments

--- jetson_uno_uart.ino
+++ jetson_uno_uart.ino
@@ -1,21 +1,33 @@
+/* Author: Park Kuyeon
+ * Title: Jetson - Arduino UNO UART Program
+ * Description: This is the code about the UNO recieving data from the Jetson
+ *              and controls the LED attatched on the UNO and sends LED status to the Jetson.
+ *              
+ * 
+ */
+
+
 int LED = 13;
+char data;
+
 
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600);
-  pinMode(LED, OUTPUT);
+  //Serial.begin(9600);   // if using Serial Monitor
+  Serial1.begin(9600);    // using TX(1), RX(0) pin
+  pinMode(LED, OUTPUT);   // LED pin mode setup
 }
-char data;
+
+
 void loop() {
   while(Serial1.available()){
-    data = (char)Serial1.read();
+    data = (char)Serial1.read();    // Receive data from the Jetson
     if(data == '1'){
       digitalWrite(LED, HIGH);
-      Serial1.print("LED ON\n");
+      Serial1.print("LED ON\n");    // Send data to the Jetson
     }
     else if(data == '0'){
       digitalWrite(LED, LOW);
-      Serial1.print("LED OFF\n");
+      Serial1.print("LED OFF\n");   // Send data to the Jetson
     }
   }
 }
